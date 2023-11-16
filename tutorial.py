02e4089671d9f92de6736502eecab0e6f65b0b6a
JessL868 <jessicaliu868@gmail.com>
2023-11-16 13:54:49 -0500

    Tutorial

---
+++ tutorial.py
@@ -0,0 +1,3 @@
+name = input("What is your username?")
+def tutorial():
+    print("To attack, input F. To heal, input heal. To engage in the fight, input P. To run away, input S. To equip your weapon or unequip your weapon, input E.")
