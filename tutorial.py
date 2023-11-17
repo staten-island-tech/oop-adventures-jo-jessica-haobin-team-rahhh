b75691537c0111466dcbd091d6cef91345eabc4e
JessL868 <jessicaliu868@gmail.com>
2023-11-17 13:58:40 -0500

    Tutorial Update (Lesson 43)

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
 name = input("What is your username?")
 def tutorial():
     print("To attack, input F. To heal, input heal. To engage in the fight, input P. To run away, input S. To equip your weapon or unequip your weapon, input E.")
+x = input(f"Now that you know the functions, what class will you choose {name}?")
