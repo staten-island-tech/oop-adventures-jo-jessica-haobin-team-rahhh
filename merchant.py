42e31cfcfdeeeb3a9a24e960681676882f4da59c
jo-svg <Josephinel43@nycstudents.net>
2023-12-02 15:42:01 -0500

    aws

--- merchant.py
+++ merchant.py
@@ -3,15 +3,14 @@ class Merchant:
         self.name = name
         self.products = products
         self.coins = coins
-    def sell(self, item, coins):
+    def sell(self, item, amount):
         self.products.remove(item)
         print(f"You have purchased {item}!")
-        
-        self.coins.append(coins)
+        self.coins.append(amount)
         return item
-    def buy(self, item, coins):
+    def buy(self, item, amount):
         self.products.append(item)
-        self.coins.remove(coins)
+        self.coins.remove(amount)
         print(f"You have sold {item}!")
 
         
