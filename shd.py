03d3ce5527b08b5e67d2d40993b48e68f1c717d8
jo-svg <Josephinel43@nycstudents.net>
2023-12-11 14:00:10 -0500

    awsd

---
+++ shd.py
@@ -0,0 +1,11 @@
+def husai():
+    user_imput = input("dhsauidah")
+    divsor = 1
+    prime = []
+    while divsor <= user_imput:
+        user_imput/divsor == 0
+        prime.append(divsor)
+        divsor += 1
+    print(prime)
+
+husai()
