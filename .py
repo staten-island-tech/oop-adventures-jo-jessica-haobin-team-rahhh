afd0c5d9e1d85c63c3ae11730d0041cdc12efb6d
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2023-12-13 13:59:53 -0500

    dfsdfsdfsdfsdfs

--- .py
+++ .py
@@ -1,18 +1,41 @@
-def factor(x):
-    global y
-    for i in range(1, x+1):
-        if x%i == 0:
-            y = print(i)
+import json
 
-factor(10)
+class enemy():
+    def __init__(self, name, health, type, damage):
+        self.name = name
+        self.health = health
+        self.type = type
+        self.damage = damage
+        
+    def __str__(self):
+        return f"{self.title}, {self.status}"
+    
 
-def prime_or_not(x):
-    global y
-    factor(x)
-    if y[0] == 1:
-        if y[1] == x:
-            print('prime')
-    else:
-        print('not prime')
 
-prime_or_not(10)
+with open('data.json', mode='r') as infile:
+     data = json.load(infile)
+
+def enemy2():
+     global monster
+     a = input('Name: ')
+     b = int(input('Health: '))
+     c = input('Type: ')
+     d = int(input('Damage: '))
+     
+     monster = enemy(a, b, c)
+     
+     data.append(
+            {
+               'name': monster.name,
+               'health': monster.health,
+               'type': monster.type
+               'damage': monster.damage
+            }
+     ) 
+          
+     with open('data.json', mode='w') as outfile:
+          json.dump(data, outfile, indent=4)
+
+
+
+enemy2()
