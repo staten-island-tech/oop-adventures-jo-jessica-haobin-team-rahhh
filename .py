1e7b9f85e315a555906f4e018414eb5874cbc355
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2023-12-14 13:37:36 -0500

    dsadads

--- .py
+++ .py
@@ -1,4 +1,3 @@
-
 import json
 
 class enemy():
@@ -9,53 +8,28 @@ class enemy():
         self.damage = damage
         
     def __str__(self):
-        return f"{self.title}, {self.status}"
+        return f"{self.name}, {self.health}, {self.type}, {self.damage}"
     
 
 
-with open('data.json', mode='r') as infile:
-     data = json.load(infile)
+with open('something.json', mode='r') as infile:
+    data = json.load(infile)
 
 def enemy2():
-     global monster
-     a = input('Name: ')
-     b = int(input('Health: '))
-     c = input('Type: ')
-     d = int(input('Damage: '))
-     
-     monster = enemy(a, b, c)
-     
-     data.append(
-            {
-               'name': monster.name,
-               'health': monster.health,
-               'type': monster.type
-               'damage': monster.damage
-            }
-     ) 
-          
-     with open('data.json', mode='w') as outfile:
-          json.dump(data, outfile, indent=4)
+    a = input('Monster name: ')
+    b = int(input('Monster health'))
+    c = input('Monster type')
+    d = int(input('Monster damage'))
 
+    monster = enemy(a, b, c, d)
 
+    data.append(
+        {
+            "Name": monster.name,
+            "Health": monster.health,
+            "Type": monster.type,
+            "Damage": monster.damage
+        }
+    )
 
-enemy2()
-c = 0
-def factor(x):
-    global c
-    for i in range(1, x+1):
-        if x%i == 0:
-            c += 1
-
-
-def prime_or_not(x):
-    global c
-    factor(x)
-    if c == 2:
-        print('prime')
-    else:
-        print('not prime')
-
-
-prime_or_not(11923)
-
+    
